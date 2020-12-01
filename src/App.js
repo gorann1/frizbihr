b20d65962842f37720906f9810a152017fc28479
goran <goran.novacic@gmail.com>
2020-12-01 12:30:12 +0100

    change_repo_stack

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import './App.css';
+import './assets/main.scss';
+import HomePage from './components/homepage/homepage.component';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div>
+      <HomePage />
     </div>
   );
 }
